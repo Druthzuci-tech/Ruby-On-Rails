7b468d2fc142762111df90d3cec2888c872a6f2d
shubha <ShubhaGJ@eshatechgroup.com>
2025-01-31 16:18:31 +0530

    created api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 module Api
     class UsersController < ApplicationController
-      skip_before_action :verify_authenticity_token  # Skip CSRF protection for API calls
+      skip_before_action :verify_authenticity_token  
   
       def create
         @user = User.new(user_params)
